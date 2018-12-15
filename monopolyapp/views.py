7180ec3879082c1b87ee93046ced3be02846a86f
yatabis <39yatabis.rim8820xxx@gmail.com>
2018-12-15 23:55:50 +0900

    test: fetch test

--- monopolyapp/views.py
+++ monopolyapp/views.py
@@ -5,7 +5,7 @@ from rest_framework import viewsets, filters
 from .models import Room, Player
 from .serializer import RoomSerializer, PlayerSerializer
 from .functions import create_room, set_new_room
-from .line import reply_text, push_text
+from .line import reply_text, push_text, HEADER
 
 # Create your views here.
 
@@ -42,8 +42,11 @@ def make_room(request):
             if Player.objects.filter(line_id=line_id).get().room_id == "":
                 Player.objects.filter(line_id=line_id).update(
                     room_id=created_room.get(), position='parent', money=2000000, deal='free')
+            else:
+                push_text(line_id, "予期せぬエラーが発生しました。")
         else:
             Player.objects.create(line_id=line_id, line_name=line_name, room_id=created_room.get(), position='parent')
+            push_text(line_id, f"ルーム{room_id[:6]}を作成しました。")
         return render(request, 'monopolyapp/room.html', {
             'title': title,
             'room_id': room_id,
@@ -55,7 +58,7 @@ def join_room(request):
     if request.method == 'GET':
         title = "部屋に入る"
         room_id = request.GET['room']
-        return render(request, 'monopolyapp/join-room.html', {'title': title, 'room_id': room_id})
+        return render(request, 'monopolyapp/join-room.html', {'title': title, 'room_id': room_id, 'header': HEADER})
     elif request.method == 'POST':
         title = "部屋に入る"
         room_id = request.POST['room-id']
@@ -76,4 +79,4 @@ class RoomViewSets(viewsets.ModelViewSet):
 class PlayerViewSets(viewsets.ModelViewSet):
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
-    filter_fields = ('room_id', 'position')
+    filter_fields = ('room_id', 'line_id', 'position')
