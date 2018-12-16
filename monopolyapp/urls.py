f1c96686a33baaca80319760c39d98eb0e6bfc4f
yatabis <39yatabis.rim8820xxx@gmail.com>
2018-12-16 20:43:58 +0900

    update: make-room LIFF

--- monopolyapp/urls.py
+++ monopolyapp/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('callback/', line_callback, name='callback'),
     path('make-room/', make_room, name='make-room'),
     path('join-room/', join_room, name='join-room'),
-    path('fetch-test/', fetch_test)
+    path('debug/rooms/'),
+    path('debug/fetch-test/', fetch_test)
 ]
 
 router = routers.DefaultRouter()
